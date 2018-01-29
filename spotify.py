611c23b223b91a6a6ae5d3465256e49825b9f59c
veliakiner <veliakiner@gmail.com>
2018-01-29 00:10:55 +0000

    Use csv module for parsing playlist.csv
    Using native `split` explodes when tracks contain commas. Python's
    csv module does some magic to avoid this.

--- spotify.py
+++ spotify.py
@@ -1,28 +1,37 @@
 import requests
-from requests.auth import HTTPBasicAuth
 import urllib
+import json
+from requests.auth import HTTPBasicAuth
 
 
 params = {"grant_type": "client_credentials"}
 
 # token = requests.post("https://accounts.spotify.com/api/token", auth=HTTPBasicAuth(*auth), data=params).text
-token = "BQBnGzw3ReuNRYEAp5EpfW9ClV0bAeNT3FqHeaP2FF6v35zXgA3m4P5Ofi7MaobYgwK-WRSsmdTj9WKiQQkhJA"
+token = "BQBY56HOjdVj7LW9JKMQC2_90hFmwOHU8WkOC5EBAs-raSPwGd5Y51Lo6raZmfEjYR-dJrtweLk22IBhwCw"
+# print token
 
 
-def whatever(track, artist):
+def generate_spotify_url(track, artist):
     url = "https://api.spotify.com/v1/search?q=track:{track}+artist:{artist}&type=track&limit=1".format(track=urllib.quote_plus(track), artist=urllib.quote_plus(artist))
-
-    result = requests.get(url, headers={'Authorization': "Bearer " + token}).text
-    import json
+    res = requests.get(url,
+                          headers={'Authorization': "Bearer " + token})
+    assert res.status_code == 200
+    result = res.text
     track = json.loads(result)["tracks"]["items"]
     if track:
         return json.loads(result)["tracks"]["items"][0]["uri"]
     return ""
-# print whatever("Being Everyone", "After Forever")
 
-with open("sample.csv") as f:
-    artists = [item.split(",")[1] for item in f.readlines()]
-    print(len(artists))
-    print(len(set(artists)))
-        # track, artist, _ = line.split(",")
-        # print whatever(track, artist)
+
+import csv
+
+
+with open("test.csv") as f:
+    track_no = len(f.readlines())
+with open("test.csv") as f:
+
+    for line_no, line in enumerate(csv.reader(f, quotechar='"',delimiter=",",quoting=csv.QUOTE_ALL)):
+        print "Writing track {} of {}".format(line_no + 1, track_no)
+        track, artist, _ = line.split(",")
+        with open("spotify_playlist.txt", "a") as f:
+            f.write(generate_spotify_url(track, artist) + "\n")

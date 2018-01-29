e866500f69f67daf5d9d4938b6c0b6473ddca532
veliakiner <veliakiner@gmail.com>
2018-01-29 00:47:15 +0000

    refresh token

--- spotify.py
+++ spotify.py
@@ -6,16 +6,24 @@ from requests.auth import HTTPBasicAuth
 
 params = {"grant_type": "client_credentials"}
 
-# token = requests.post("https://accounts.spotify.com/api/token", auth=HTTPBasicAuth(*auth), data=params).text
-token = "BQBY56HOjdVj7LW9JKMQC2_90hFmwOHU8WkOC5EBAs-raSPwGd5Y51Lo6raZmfEjYR-dJrtweLk22IBhwCw"
-# print token
+token_json = requests.post("https://accounts.spotify.com/api/token", auth=HTTPBasicAuth(*auth), data=params).text
+token = json.loads(token_json)["access_token"]
 
 
-def generate_spotify_url(track, artist):
+def generate_spotify_url(track, artist, retry_attempted=False):
     url = "https://api.spotify.com/v1/search?q=track:{track}+artist:{artist}&type=track&limit=1".format(track=urllib.quote_plus(track), artist=urllib.quote_plus(artist))
     res = requests.get(url,
                           headers={'Authorization': "Bearer " + token})
-    assert res.status_code == 200
+    if res.status_code != 200:
+        if res.status_code == 401:
+            print "Refreshing token"
+            if retry_attempted:
+                raise RuntimeError("Oh shit")
+            global token
+            token_json = requests.post("https://accounts.spotify.com/api/token", auth=HTTPBasicAuth(*auth), data=params).text
+            token = json.loads(token_json)["access_token"]
+            if not retry_attempted:
+                return generate_spotify_url(track, artist, True)
     result = res.text
     track = json.loads(result)["tracks"]["items"]
     if track:
@@ -26,12 +34,16 @@ def generate_spotify_url(track, artist):
 import csv
 
 
-with open("test.csv") as f:
+with open("wew.csv") as f:
     track_no = len(f.readlines())
-with open("test.csv") as f:
+with open("wew.csv") as f:
 
     for line_no, line in enumerate(csv.reader(f, quotechar='"',delimiter=",",quoting=csv.QUOTE_ALL)):
         print "Writing track {} of {}".format(line_no + 1, track_no)
-        track, artist, _ = line.split(",")
+        track, artist, _ = line
         with open("spotify_playlist.txt", "a") as f:
-            f.write(generate_spotify_url(track, artist) + "\n")
+            url = generate_spotify_url(track, artist)
+            if url:
+                f.write(generate_spotify_url(track, artist) + "\n")
+            else:
+                print "Track not found on Spotify: {} - {}".format(artist, track)

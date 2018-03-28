075b20f71545938e1d9d4ddb0f2818ddfe9e933e
veliakiner <veliakiner@gmail.com>
2018-03-28 20:30:46 +0100

    spotify: Make Python3-friendly and make formatting changes

--- spotify.py
+++ spotify.py
@@ -1,8 +1,11 @@
 import requests
-import urllib
+from urllib.parse import quote_plus
 import json
 from requests.auth import HTTPBasicAuth
 import os
+import csv
+import re
+
 
 SPOTIFY_KEY = os.environ.get("SPOTIFY_KEY")
 SPOTIFY_SECRET = os.environ.get("SPOTIFY_SECRET")
@@ -17,12 +20,12 @@ token = json.loads(token_json)["access_token"]
 
 
 def generate_spotify_url(track, artist, retry_attempted=False):
-    url = "https://api.spotify.com/v1/search?q=track:{track}+artist:{artist}&type=track&limit=1".format(track=urllib.quote_plus(track), artist=urllib.quote_plus(artist))
+    url = "https://api.spotify.com/v1/search?q=track:{track}+artist:{artist}&type=track&limit=1".format(track=quote_plus(track), artist=quote_plus(artist))
     res = requests.get(url,
-                          headers={'Authorization': "Bearer " + token})
+                       headers={'Authorization': "Bearer " + token})
     if res.status_code != 200:
         if res.status_code == 401:
-            print "Refreshing token"
+            print("Refreshing token")
             if retry_attempted:
                 raise RuntimeError("Oh shit")
             global token
@@ -37,10 +40,6 @@ def generate_spotify_url(track, artist, retry_attempted=False):
     return ""
 
 
-import csv
-import re
-
-
 with open("wew.csv") as f:
     track_no = len(f.readlines())
     # files = f.readlines()
@@ -50,23 +49,23 @@ with open("spotify_playlist.txt", "w") as f:
 with open("wew.csv") as f:
     for line_no, line in enumerate(csv.reader(f, quotechar='"',delimiter=",",quoting=csv.QUOTE_ALL)):
         try:
-            print "Writing track {} of {}".format(line_no + 1, track_no)
+            print("Writing track {} of {}".format(line_no + 1, track_no))
             track, artist, _ = line
             url = generate_spotify_url(track, artist)
             if not url:
-                print "Track not found on Spotify: {} - {}".format(artist, track)
+                print("Track not found on Spotify: {} - {}".format(artist, track))
                 pattern = r'{}[ -]*'.format(artist)
                 artist_crap = re.findall(pattern, track, re.IGNORECASE)
                 if artist_crap:
                     track = track.replace(artist_crap[0], "")
-                    print "Artist found in track. Correcting possible scrobble error. Searching for {} - {}".format(artist, track),
+                    print("Artist found in track. Correcting possible scrobble error. Searching for {} - {}".format(artist, track),)
                     url = generate_spotify_url(track, artist)
                     if url:
-                        print "- Correction worked."
+                        print("- Correction worked.")
                     else:
-                        print ""
+                        print("")
             if url:
                 with open("spotify_playlist.txt", "a") as g:
                     g.write(generate_spotify_url(track, artist) + "\n")
         except Exception as e:
-            print e
+            print(e)
